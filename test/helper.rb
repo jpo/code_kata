dac0b060a8a7d7244a18267e56eb459ba6124e4e
Josh O'Rourke <joshorourke@me.com>
2016-08-19 21:58:46 -0400

    Fix long line

--- test/helper.rb
+++ test/helper.rb
@@ -2,4 +2,6 @@ require 'minitest/autorun'
 require 'minitest/unit'
 require 'minitest/spec'
 
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'programming_problems'))
+require File.expand_path(
+  File.join(File.dirname(__FILE__), '..', 'lib', 'programming_problems')
+)
