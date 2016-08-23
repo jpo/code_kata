884041c8fabc906a26de0278bdc5e818c7203e64
Josh O'Rourke <joshorourke@me.com>
2016-08-23 18:36:37 -0400

    Wrap all tests in a 'describe' block

--- test/test_binary_tree.rb
+++ test/test_binary_tree.rb
@@ -3,17 +3,31 @@ require 'helper'
 module ProgrammingProblems
   class BinaryTreeTest < MiniTest::Test
     describe 'BinaryTree' do
-      it 'initializes empty' do
-        tree = BinaryTree.new
-        assert tree.root.nil?
+      describe 'initialize' do
+        it 'is empty' do
+          tree = BinaryTree.new
+          assert tree.root.nil?
+        end
       end
 
-      it 'inserts a node' do
-        tree = BinaryTree.new
-        tree.insert(1)
-        assert_equal 1, tree.root.data
-        assert tree.root.left.nil?
-        assert tree.root.right.nil?
+      describe 'insert' do
+        it 'inserts a node' do
+          tree = BinaryTree.new
+          tree.insert(1)
+          assert_equal 1, tree.root.data
+          assert tree.root.left.nil?
+          assert tree.root.right.nil?
+        end
+
+        it 'sorts nodes on insert' do
+          tree = BinaryTree.new
+          [3, 1, 2, 4, 5].each { |n| tree.insert(n) }
+          assert_equal 3, tree.root.data
+          assert_equal 1, tree.root.left.data
+          assert_equal 2, tree.root.left.right.data
+          assert_equal 4, tree.root.right.data
+          assert_equal 5, tree.root.right.right.data
+        end
       end
 
       describe 'remove' do
@@ -50,37 +64,33 @@ module ProgrammingProblems
         end
       end
 
-      it 'sorts nodes on insert' do
-        tree = BinaryTree.new
-        [3, 1, 2, 4, 5].each { |n| tree.insert(n) }
-        assert_equal 3, tree.root.data
-        assert_equal 1, tree.root.left.data
-        assert_equal 2, tree.root.left.right.data
-        assert_equal 4, tree.root.right.data
-        assert_equal 5, tree.root.right.right.data
+      describe 'find_value' do
+        it 'finds a node by value' do
+          tree = BinaryTree.new
+          (1..5).each { |n| tree.insert(n) }
+          assert_equal 3, tree.find_value(3).data
+          assert tree.find_value(0).nil?
+        end
       end
 
-      it 'finds a node by value' do
-        tree = BinaryTree.new
-        (1..5).each { |n| tree.insert(n) }
-        assert_equal 3, tree.find_value(3).data
-        assert tree.find_value(0).nil?
+      describe 'find_parent' do
+        it 'finds the parent of a given node' do
+          tree = BinaryTree.new
+          (1..5).each { |n| tree.insert(n) }
+          child = tree.root.right
+          assert_equal 2, child.data
+          assert_equal 1, tree.find_parent(child).data
+        end
       end
 
-      it 'finds the parent of a given node' do
-        tree = BinaryTree.new
-        (1..5).each { |n| tree.insert(n) }
-        child = tree.root.right
-        assert_equal 2, child.data
-        assert_equal 1, tree.find_parent(child).data
-      end
-
-      it 'finds the successor of a given node' do
-        tree = BinaryTree.new
-        (1..5).each { |n| tree.insert(n) }
-        parent = tree.root
-        assert_equal 1, parent.data
-        assert_equal 2, tree.find_successor(parent).data
+      describe 'find_successor' do
+        it 'finds the successor of a given node' do
+          tree = BinaryTree.new
+          (1..5).each { |n| tree.insert(n) }
+          parent = tree.root
+          assert_equal 1, parent.data
+          assert_equal 2, tree.find_successor(parent).data
+        end
       end
 
       describe 'size' do
