1293e6ffd5ad7bc595792295e841427ab1f49ed9
Josh O'Rourke <joshorourke@me.com>
2016-09-16 23:32:41 -0500

    Add size tests for Heap

--- test/test_heap.rb
+++ test/test_heap.rb
@@ -10,6 +10,18 @@ module CodeKata
           assert_equal 0, heap.size
         end
       end
+
+      describe 'size' do
+        it 'is zero when the heap is empty' do
+          heap = Heap.new([])
+          assert_equal 0, heap.size
+        end
+
+        it 'is equal to number of items in the heap' do
+          heap = Heap.new([1, 2, 3])
+          assert_equal 3, heap.size
+        end
+      end
     end
   end
 end
