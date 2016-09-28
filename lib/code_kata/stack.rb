51c0f5efe4fde2c9338f4c56244ffca0e070aa5e
Josh O'Rourke <joshorourke@me.com>
2016-09-27 22:31:48 -0400

    Add docs for Stack class

--- lib/code_kata/stack.rb
+++ lib/code_kata/stack.rb
@@ -1,15 +1,19 @@
 require 'code_kata/linked_list'
 
 module CodeKata
+  # Represents a last-in, first-out collection of objects.
   class Stack
     def initialize
       @list = LinkedList.new
     end
 
+    # Pushes data to the top of the stack.
     def push(data)
       @list.insert(data)
     end
 
+    # Retrieves data from the top of the stack. If the stack is empty,
+    # RuntimeError is raised.
     def pop
       raise 'No items in stack' unless @list.head
       result     = @list.head.data
@@ -17,11 +21,13 @@ module CodeKata
       result
     end
 
+    # Retrieves data from the top of the stack without removing it.
     def peek
       raise 'No items in stack' unless @list.head
       @list.head.data
     end
 
+    # Returns true if the stack is empty.
     def empty?
       @list.head.nil?
     end
