848ff635226f2247bb7aed6e41ce7444b00d128e
Josh O'Rourke <joshorourke@me.com>
2016-09-27 22:27:42 -0400

    Add docs to Queue class

--- lib/code_kata/queue.rb
+++ lib/code_kata/queue.rb
@@ -1,11 +1,13 @@
 require 'code_kata/queue_node'
 
 module CodeKata
+  # Represents a first-in, first-out collection of objects.
   class Queue
     def initialize
       @head = @tail = nil
     end
 
+    # Pushes data to the queue.
     def push(data)
       if @tail
         @tail.next = QueueNode.new(data)
@@ -15,6 +17,8 @@ module CodeKata
       end
     end
 
+    # Retrieves data from the queue. If the queue is empty, RuntimeError is
+    # raised.
     def pop
       raise 'No items to pop' unless @head
       result = @head.data
@@ -23,6 +27,7 @@ module CodeKata
       result
     end
 
+    # Returns true if the queue is empty.
     def empty?
       @head.nil?
     end
